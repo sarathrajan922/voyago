389d2baec36cccb775385f5be441987956478ab7
sarathrajan922 <sarathrajan922@gmail.com>
2023-08-08 20:40:42 +0530

    run command modified

--- client/src/components/user/Navbar/Navbar.jsx
+++ client/src/components/user/Navbar/Navbar.jsx
@@ -309,72 +309,6 @@ function ProfileMenu() {
 //   },
 // ];
 
-function NavListMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const triggers = {
-    onMouseEnter: () => setIsMenuOpen(true),
-    onMouseLeave: () => setIsMenuOpen(false),
-  };
-
-  // const renderItems = navListMenuItems.map(({ title, description }) => (
-  //   <a href="#" key={title}>
-  //     <MenuItem>
-  //       <Typography variant="h6" color="blue-gray" className="mb-1">
-  //         {title}
-  //       </Typography>
-  //       <Typography variant="small" color="gray" className="font-normal">
-  //         {description}
-  //       </Typography>
-  //     </MenuItem>
-  //   </a>
-  // ));
-
-  return (
-    <React.Fragment>
-      {/* <Menu open={isMenuOpen} handler={setIsMenuOpen}>
-        <MenuHandler>
-          <Typography as="a" href="#" variant="small" className="font-normal">
-            <MenuItem
-              {...triggers}
-              className="hidden items-center gap-2 text-blue-gray-900 lg:flex lg:rounded-full"
-            >
-              <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Pages{" "}
-              <ChevronDownIcon
-                strokeWidth={2}
-                className={`h-3 w-3 transition-transform ${
-                  isMenuOpen ? "rotate-180" : ""
-                }`}
-              />
-            </MenuItem>
-          </Typography>
-        </MenuHandler>
-        <MenuList
-          {...triggers}
-          className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid"
-        >
-          <Card
-            color="blue"
-            shadow={false}
-            variant="gradient"
-            className="col-span-3 grid h-full w-full place-items-center rounded-md"
-          >
-            <RocketLaunchIcon strokeWidth={1} className="h-28 w-28" />
-          </Card>
-          <ul className="col-span-4 flex w-full flex-col gap-1">
-            {renderItems}
-          </ul>
-        </MenuList>
-      </Menu>
-      <MenuItem className="flex items-center gap-2 text-blue-gray-900 lg:hidden">
-        <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Pages{" "}
-      </MenuItem>
-      <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
-        {renderItems}
-      </ul> */}
-    </React.Fragment>
-  );
-}
 
 
 
@@ -388,7 +322,7 @@ function NavList() {
       icon: UserCircleIcon,
       action: () => {
         // Action for Account
-        console.log("Account clicked");
+      
          navigate('/profile')
         
       },
@@ -398,7 +332,7 @@ function NavList() {
       icon: CubeTransparentIcon,
       action: () => {
         // Action for Account
-        console.log("community clicked");
+        
         navigate('/community')
         
       },
@@ -408,7 +342,7 @@ function NavList() {
       icon: PhoneIcon,
       action: () => {
         // Action for Account
-        console.log("contact clicked");
+        
       },
     },
     
@@ -416,7 +350,7 @@ function NavList() {
   
   return (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
-      <NavListMenu />
+      {/* <NavListMenu /> */}
       {navListItems.map(({ label, icon,action }, key) => (
 
         
@@ -446,7 +380,7 @@ export default function ComplexNavbar() {
   const [isNavOpen, setIsNavOpen] = React.useState(false);
   const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
   const userData = useSelector(selectUser)
-  console.log(userData)
+  
   //! if user login isloggedin is true
 //  const [isloggedin,setIsloggedin]= useState(null)
  const userToken = localStorage.getItem('userToken')
--- client/src/components/user/Pages/Carousel.tsx
+++ client/src/components/user/Pages/Carousel.tsx
@@ -1,136 +1,18 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 
 import React from "react";
-import { Carousel, Typography, Button } from "@material-tailwind/react";
+import { Typography, Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
- 
-// export default function ManiCarousel() {
-//   return (
-//     <Carousel className="rounded-xl">
-//       <div className="relative h-fit w-full">
-//         <img
-//           src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-//           alt="image 1"
-//           className="h-fit w-full object-cover"
-//         />
-//         <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
-//           <div className="w-3/4 text-center md:w-2/4">
-//             <Typography
-//               variant="h1"
-//               color="white"
-//               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-//             >
-//               The Beauty of Nature
-//             </Typography>
-//             <Typography
-//               variant="lead"
-//               color="white"
-//               className="mb-12 opacity-80"
-//             >
-//               It is not so much for its beauty that the forest makes a claim
-//               upon men&apos;s hearts, as for that subtle something, that
-//               quality of air that emanation from old trees, that so
-//               wonderfully changes and renews a weary spirit.
-//             </Typography>
-//             <div className="flex justify-center gap-2">
-//               <Button size="lg" color="white">
-//                 Explore
-//               </Button>
-//               <Button size="lg" color="white" variant="text">
-//                 Search
-//               </Button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="relative h-full w-full">
-//         <img
-//           src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-//           alt="image 2"
-//           className="h-full w-full object-cover"
-//         />
-//         <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
-//           <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
-//             <Typography
-//               variant="h1"
-//               color="white"
-//               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-//             >
-//               The Beauty of Nature
-//             </Typography>
-//             <Typography
-//               variant="lead"
-//               color="white"
-//               className="mb-12 opacity-80"
-//             >
-//               It is not so much for its beauty that the forest makes a claim
-//               upon men&apos;s hearts, as for that subtle something, that
-//               quality of air that emanation from old trees, that so
-//               wonderfully changes and renews a weary spirit.
-//             </Typography>
-//             <div className="flex gap-2">
-//               <Button size="lg" color="white">
-//                 Explore
-//               </Button>
-//               <Button size="lg" color="white" variant="text">
-//                 Search
-//               </Button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-
-//       <div className="relative h-full w-full">
-//         <img
-//           src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-//           alt="image 3"
-//           className="h-full w-full object-cover"
-//         />
-//         <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
-//           <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
-//             <Typography
-//               variant="h1"
-//               color="white"
-//               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-//             >
-//               The Beauty of Nature
-//             </Typography>
-//             <Typography
-//               variant="lead"
-//               color="white"
-//               className="mb-12 opacity-80"
-//             >
-//               It is not so much for its beauty that the forest makes a claim
-//               upon men&apos;s hearts, as for that subtle something, that
-//               quality of air that emanation from old trees, that so
-//               wonderfully changes and renews a weary spirit.
-//             </Typography>
-//             <div className="flex gap-2">
-//               <Button size="lg" color="white">
-//                 Explore
-//               </Button>
-//               <Button size="lg" color="white" variant="text">
-//                 Gallery
-//               </Button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </Carousel>
-//   );
-// }
-
-
-
 
 
 interface CarouselProps {
   images: Record<string, string>; // Define the type for the 'images' object
 }
 
+
+
 export const SecondaryCarousel: React.FC<CarouselProps> = ({ images }) => {
+  
   return (
     <div
       id="default-carousel"
@@ -145,7 +27,6 @@ export const SecondaryCarousel: React.FC<CarouselProps> = ({ images }) => {
             data-carousel-item
             key={index}
           >
-         
             <img
               src={images[key]}
               className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
@@ -153,8 +34,8 @@ export const SecondaryCarousel: React.FC<CarouselProps> = ({ images }) => {
             />
           </div>
         ))}
-      
-         <div className="absolute z-20 inset-0 grid h-full w-full  items-center bg-black/75">
+
+        <div className="absolute z-20 inset-0 grid h-full w-full  items-center bg-black/75">
           <div className="w-3/4 pl-12 md:w-3/4 md:pl-20 lg:pl-24">
             <Typography
               variant="h1"
@@ -166,30 +47,24 @@ export const SecondaryCarousel: React.FC<CarouselProps> = ({ images }) => {
             <Typography
               variant="lead"
               color="white"
-              className="mb-12 opacity-80 md:text-xl text-sm"
+              className="mb-12 opacity-80 md:text-xl text-sm "
             >
-              It is not so much for its beauty that the forest makes a claim
-              upon men&apos;s hearts, as for that subtle something, that
-              quality of air that emanation from old trees
+              Discover your dream destinations with our travel guides. Unveil
+              breathtaking landscapes, vibrant cultures, and hidden gems
+              worldwide. Let's embark on an unforgettable adventure together!
             </Typography>
             <div className="flex gap-2">
               <Button size="sm" color="white">
                 Explore
               </Button>
-                <Link to='/search'>
-              <Button size="sm" color="white" variant="text">
-                 Search
-              </Button>
-                </Link>
+              <Link to="/search">
+                <Button size="sm" color="white" variant="text">
+                  Search
+                </Button>
+              </Link>
             </div>
           </div>
         </div>
-
-
-
-
-
-
       </div>
       {/* Slider indicators */}
       <div className="absolute  flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
@@ -222,5 +97,3 @@ export const SecondaryCarousel: React.FC<CarouselProps> = ({ images }) => {
     </div>
   );
 };
-
-
