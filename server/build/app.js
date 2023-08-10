0ab5decbfca5d679249c667849f0cefa98aeead9
sarathrajan922 <sarathrajan922@gmail.com>
2023-08-11 01:12:09 +0530

    build file changedddddddd

--- server/build/app.js
+++ server/build/app.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("./frameworks/webserver/express"));
 const routes_1 = __importDefault(require("./frameworks/webserver/routes"));
