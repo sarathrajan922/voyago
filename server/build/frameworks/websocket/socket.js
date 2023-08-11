d137a6242d582b5ace681f243896169a7fc07286
sarathrajan922 <sarathrajan922@gmail.com>
2023-08-11 20:15:54 +0530

    socket file removed

---
+++ server/build/frameworks/websocket/socket.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// import {
+//   ClientToServerEvents,
+//   InterServerEvents,
+//   ServerToClientEvents,
+//   SocketData,
+// } from '../../types/socketInterfaces';
+const socketConfig = (io) => {
+    let users = [];
+    const addUsers = (userId, socketId) => {
+        !users.some(user => user.userId === userId) && users.push({ userId, socketId });
+    };
+    const removeUsers = (socketId) => {
+        users = users.filter((user => user.socketId !== socketId));
+    };
+    const getUser = (userId) => {
+        return users.find((user) => user.userId === userId);
+    };
+    io.on('connection', (socket) => {
+        // when connect
+        console.log('a user connected...');
+        io.emit('welcome', "hello this is socket server!");
+        socket.on('addUser', (userId) => {
+            addUsers(userId, socket.id);
+            io.emit('getUsers', users);
+        });
+        // send and get message
+        socket.on('sendMessage', (data) => {
+            console.log(data);
+            io.emit('getMessage', {
+                senderId: data.userId,
+                message: data.message,
+                communityId: data.communityIds
+            });
+        });
+        // when disconnect
+        socket.on('disconnect', () => {
+            console.log('a user disconnected!');
+            removeUsers(socket.id);
+            io.emit('getUsers', users);
+        });
+    });
+};
+exports.default = socketConfig;
--- server/src/app.ts
+++ server/src/app.ts
@@ -8,12 +8,23 @@ import serverConfig from './frameworks/webserver/server'
 import express, {Application} from 'express'
 import connectDB from './frameworks/database/mongodb/connection'
 import errorHandlingMiddleware from './frameworks/webserver/middlewares/errorHandle'
-
+import socketConfig from './frameworks/websocket/socket';
+import { Server } from 'socket.io';
 const app:Application = express()
 expressConfig(app)
 //connecting mongoDb
 connectDB();
 const server = http.createServer(app)
+
+//* web socket connection
+const io = new Server<any>(server,{
+    cors:{
+        origin:true,
+        methods:["GET","POST"]
+    } 
+  });
+  
+  socketConfig(io)
 router(app)
 app.use(errorHandlingMiddleware)
 serverConfig(server).startServer()
