ffb270fc96f0e445f17ea54e00e351f88af1e640
sarathrajan922 <sarathrajan922@gmail.com>
2023-08-11 14:35:31 +0530

    socket connection url changedd

--- client/src/components/user/chat/Community.tsx
+++ client/src/components/user/chat/Community.tsx
@@ -140,7 +140,7 @@ const Community: React.FC = () => {
 
   useEffect(() => {
     // socket.current = io("ws://localhost:8900");
-    socket.current = io("wss://voyago.site/socket/");
+    socket.current = io("https://voyago.site/socket/");
   }, []);
 
   useEffect(() => {
